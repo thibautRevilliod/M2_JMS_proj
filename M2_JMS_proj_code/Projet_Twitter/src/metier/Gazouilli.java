1cc3f471c06c00a6c388506195b4e53ea6b38174
Thibaut Revilliod <thibaut.revilliod@gmail.com>
2015-11-06 09:48:27 +0100

    Gazouilli class

---
+++ M2_JMS_proj_code/Projet_Twitter/src/metier/Gazouilli.java
@@ -0,0 +1,30 @@
+package metier;
+
+public class Gazouilli {
+	
+	private String contenu;
+	private String date;
+	private String heure;
+	private String ville;
+	private String emetteur;
+	
+	
+
+	public Gazouilli(String contenu, String date, String heure, String ville,
+			String emetteur) {
+		super();
+		this.contenu = contenu;
+		this.date = date;
+		this.heure = heure;
+		this.ville = ville;
+		this.emetteur = emetteur;
+	}
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
