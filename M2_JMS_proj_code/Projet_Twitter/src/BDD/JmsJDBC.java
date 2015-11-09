1ce4a10f0307b132325efc386c958753526a4c5d
lmpmrsx <landry.molinier@gmail.com>
2015-11-09 11:36:12 +0100

    BDD v2

--- M2_JMS_proj_code/Projet_Twitter/src/BDD/JmsJDBC.java
+++ M2_JMS_proj_code/Projet_Twitter/src/BDD/JmsJDBC.java
@@ -20,33 +20,34 @@ public class JmsJDBC {
 	        Statement s = conn.createStatement();
 			
         	s.execute("create table IF NOT EXISTS PROFIL  ( " +
-        			" idPROFIL VARCHAR( 500 ) NOT NULL PRIMARY KEY, " +
+        			" idPROFIL INT NOT NULL PRIMARY KEY, " +
+        			" PSEUDO VARCHAR( 500 ) , " +
         			" NOM VARCHAR( 500 ) , " +
 					" PRENOM VARCHAR( 500 ) , " +
 					" VILLE VARCHAR( 500 ) )");
 
         	s.execute("create table IF NOT EXISTS GAZOUILLI  ( " +
-        			" idGAZOUILLI VARCHAR( 500 ) NOT NULL PRIMARY KEY, " +
+        			" idGAZOUILLI INT NOT NULL PRIMARY KEY, " +
         			" CONTENU VARCHAR( 500 ) , " +
 					" DATEHEURE TIMESTAMP , " +
         			" VILLE VARCHAR( 500 ) , " +
-					" EMETTEUR VARCHAR( 500 ) , " +
+					" EMETTEUR INT , " +
 					" FOREIGN KEY (EMETTEUR) REFERENCES PROFIL(idPROFIL))");
 			
         	s.execute("create table IF NOT EXISTS ABONNEMENTS  ( " +
-        			" idABONNEMENTS VARCHAR( 500 ) NOT NULL PRIMARY KEY, " +
+        			" idABONNEMENTS INT NOT NULL PRIMARY KEY, " +
 					" DATEHEURE TIMESTAMP )");
 	        
         	s.execute("create table IF NOT EXISTS ABONNE  ( " +
-        			" idABONNE VARCHAR( 500 ) NOT NULL, " +
-					" idPROFIL VARCHAR( 500 ) NOT NULL, " +
+        			" idABONNE INT NOT NULL, " +
+					" idPROFIL INT NOT NULL, " +
 					" PRIMARY KEY (idABONNE, idPROFIL) , " +
 					" FOREIGN KEY (idABONNE) REFERENCES ABONNEMENTS(idABONNEMENTS) , " +
 					" FOREIGN KEY (idPROFIL) REFERENCES PROFIL(idPROFIL))");
 	        
         	s.execute("create table IF NOT EXISTS SUIVI  ( " +
-        			" idSUIVI VARCHAR( 500 ) NOT NULL, " +
-					" idPROFIL VARCHAR( 500 ) NOT NULL, " +
+        			" idSUIVI INT NOT NULL, " +
+					" idPROFIL INT NOT NULL, " +
 					" PRIMARY KEY (idSUIVI, idPROFIL) , " +
 					" FOREIGN KEY (idSUIVI) REFERENCES ABONNEMENTS(idABONNEMENTS) , " +
 					" FOREIGN KEY (idPROFIL) REFERENCES PROFIL(idPROFIL))");
@@ -120,39 +121,52 @@ public class JmsJDBC {
 		return res;
 	}
 	
-	public boolean creerProfil(String pid, String pnom, String pprenom, String pville) {
+	public int creerProfil(String ppseudo, String pnom, String pprenom, String pville) {
+		int id = -1;
 		try {
 			Statement s = conn.createStatement();
-			ResultSet rs = s.executeQuery("select idPROFIL from PROFIL where idPROFIL = '"+pid+"'");
-	        if (rs.next()) {
-	        	// un compte existe deja avec cet id
-	        	return false;
-	        } else {
-	        	s.executeUpdate("insert into PROFIL values ('"+pid+"', '"+pnom+"','"+pprenom+"','"+pville+"')");
-	        	return true;
+        	//récupère le dernier ID
+			ResultSet rs = s.executeQuery("select MAX(idPROFIL) from PROFIL GROUP BY idPROFIL");
+        	if (rs.next())
+        	{
+        		id = rs.getInt(1)+1;
+	        	s.executeUpdate("insert into PROFIL values ('"+id+"', '"+ppseudo+"', '"+pnom+"','"+pprenom+"','"+pville+"')");
+	        } 
+			else
+	        {
+				id = 1;
+	        	s.executeUpdate("insert into PROFIL values ('1', '"+ppseudo+"', '"+pnom+"','"+pprenom+"','"+pville+"')");
 	        }
+	        
+	        return id;
 		} catch (SQLException e) {
 			e.printStackTrace();
-			return false;
+			return id;
 		}
 		
 		
 	}
 	
-	public boolean creerGazouilli(String pid, String pcontenu, String pville, String pemetteur) {
+	public int creerGazouilli(String pcontenu, String pville, String pemetteur) {
+		int id = -1;
 		try {
 			Statement s = conn.createStatement();
-			ResultSet rs = s.executeQuery("select idGAZOUILLI from GAZOUILLI where idGAZOUILLI = '"+pid+"'");
-	        if (rs.next()) {
-	        	// un compte existe deja avec cet id
-	        	return false;
-	        } else {
-	        	s.executeUpdate("insert into GAZOUILLI values ('"+pid+"', '"+pcontenu+"',CURRENT_TIMESTAMP(),'"+pville+"','"+pemetteur+"')");
-	        	return true;
-	        }
+        	//récupère le dernier ID
+			ResultSet rs = s.executeQuery("select MAX(idGAZOUILLI) from GAZOUILLI GROUP BY idGAZOUILLI");
+        	if (rs.next())
+        	{
+        		id = rs.getInt(1)+1;
+        		s.executeUpdate("insert into GAZOUILLI values ('"+id+"', '"+pcontenu+"',CURRENT_TIMESTAMP(),'"+pville+"','"+pemetteur+"')");
+        	}
+        	else
+        	{
+        		id = 1;
+        		s.executeUpdate("insert into GAZOUILLI values ('1', '"+pcontenu+"',CURRENT_TIMESTAMP(),'"+pville+"','"+pemetteur+"')");
+        	}
+        	return id;
 		} catch (SQLException e) {
 			e.printStackTrace();
-			return false;
+			return id;
 		}
 	}
 
@@ -226,15 +240,16 @@ public class JmsJDBC {
 	}
 	
 	public static void main(String[] args) throws Exception {
-		//JmsJDBC.clearBDD("JMS");
+		JmsJDBC.clearBDD("JMS");
 		JmsJDBC bdd = new JmsJDBC("JMS");
 		
 		System.out.println(" Création des profils : ");
-		bdd.creerProfil("1", "NomToto", "PrenomToto", "Toulouse");
-		bdd.creerProfil("2", "NomTutu", "PrenomTutu", "Paris");
+		System.out.println("Profil n°: " + bdd.creerProfil("PseudoToto", "NomToto", "PrenomToto", "Toulouse"));
+		System.out.println("Profil n°: " +bdd.creerProfil("PseudoTutu", "NomTutu", "PrenomTutu", "Paris"));
 		System.out.println(" --> OK");
 		System.out.println(" Création des Gazouilli : ");
-		bdd.creerGazouilli("1", "Bonjour contenu", "Toulouse", "1");
+		System.out.println("gazouilli n°: " + bdd.creerGazouilli("Bonjour contenu", "Toulouse", "1"));
+		System.out.println("gazouilli n°: " + bdd.creerGazouilli("Bonjour contenu2", "Toulouse2", "1"));
 		System.out.println(" --> OK");
 		
 //		banque.creerCompte("Bobby", 1000);
