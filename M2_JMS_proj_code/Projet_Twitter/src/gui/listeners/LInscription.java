eed9fe99c7c2522472404c8b23183d0b04ce12e6
MelT <melanietanguy.ch@gmail.com>
2015-11-10 18:24:39 +0100

    Ajout de pop-up confirmation inscription

--- M2_JMS_proj_code/Projet_Twitter/src/gui/listeners/LInscription.java
+++ M2_JMS_proj_code/Projet_Twitter/src/gui/listeners/LInscription.java
@@ -7,6 +7,8 @@ import jms.SenderTwitter;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
+
 public class LInscription implements ActionListener
 {
 
@@ -20,7 +22,15 @@ public class LInscription implements ActionListener
 	public void actionPerformed(ActionEvent e)
 	{
 		System.out.println("OK j'ai cliqué sur m'inscrire");
+		
 		SenderTwitter.inscription("Toto", "123", "Nom", "Prenom", "Ville");
+		
+		String message = "!celui qu'on reçoit";
+		/**
+		 * TODO : Ajouter : SenderTwitter.getMessage()...
+		 * Puis ouvrir la pop-up
+		 * */
+		JOptionPane.showMessageDialog(vi, message, "Information Inscription", JOptionPane.INFORMATION_MESSAGE);
 	}
 }
 
